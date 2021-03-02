6b3b75875c29661e232b9d31736b44766f1a570a
Chris Richards <chris.richards@yellowfeather.co.uk>
2021-03-02 09:44:22 +0000

    Changes authorization level to system

--- functions/Parse.cs
+++ functions/Parse.cs
@@ -14,7 +14,7 @@ namespace AisParser.net
     {
         [FunctionName("parse")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.System, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             string aisMessage = req.Query["aisMessage"];
