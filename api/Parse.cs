22b3abcb1e85ccbb15128d37a18aaa77116559b9
Chris Richards <chris.richards@yellowfeather.co.uk>
2022-12-02 21:38:27 +0000

    Upgrades to net7.0

--- api/Parse.cs
+++ api/Parse.cs
@@ -1,58 +1,77 @@
 using System;
-using System.IO;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
-namespace AisParser.net
+namespace AisParser.net;
+
+public class HttpTrigger
 {
-    public static class Parse
+    public class Query
     {
-        [FunctionName("parse")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            string aisMessage = req.Query["aisMessage"];
+        public string AisMessage { get; set; }
+    }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            aisMessage = aisMessage ?? data?.aisMessage;
+    private readonly ILogger _logger;
 
-            if (string.IsNullOrEmpty(aisMessage)) {
-                var problemDetails = new ProblemDetails
-                {
-                    Status = StatusCodes.Status400BadRequest,
-                    Type = "https://httpstatuses.com/400",
-                    Title = "aisMessage not specified.",
-                    Detail = "Specify the aisMessage to be decoded in the request body.",
-                    Instance = req.Path
-                };
-                return new BadRequestObjectResult(problemDetails);
-            }
+    public HttpTrigger(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<HttpTrigger>();
+    }
 
-            try {
-                var parser = new AisParser.Parser();
-                var message = parser.Parse(aisMessage);
-                return new OkObjectResult(message);
-            }
-            catch (Exception ex) {
-                var problemDetails = new ProblemDetails
-                {
-                    Status = StatusCodes.Status422UnprocessableEntity,
-                    Type = "https://httpstatuses.com/422",
-                    Title = "An error occurred parsing the specified aisMessage.",
-                    Detail = ex.Message,
-                    Instance = req.Path
-                };
+    [Function("parse")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+    {
+        var query = await JsonSerializer.DeserializeAsync<Query>(req.Body);
+        var aisMessage = query?.AisMessage;
 
-                log.LogError($"Exception parsing message: {aisMessage}", ex);
-                return new UnprocessableEntityObjectResult(problemDetails);
-            }
+        if (string.IsNullOrEmpty(aisMessage)) {
+            var problemDetails = new ProblemDetails
+            {
+                Status = HttpStatusCode.BadRequest,
+                Type = "https://httpstatuses.com/400",
+                Title = "aisMessage not specified.",
+                Detail = "Specify the aisMessage to be decoded in the request body.",
+                Instance = req.Url.AbsolutePath
+            };
+            var response = req.CreateResponse(HttpStatusCode.BadRequest);
+            await response.WriteAsJsonAsync(problemDetails);
+            return response;
+        }
+
+        try {
+            var parser = new Parser();
+            var message = parser.Parse(aisMessage);
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            await response.WriteAsJsonAsync(message);
+            return response;
+        }
+        catch (Exception ex) {
+            var problemDetails = new ProblemDetails
+            {
+                Status = HttpStatusCode.UnprocessableEntity,
+                Type = "https://httpstatuses.com/422",
+                Title = "An error occurred parsing the specified aisMessage.",
+                Detail = ex.Message,
+                Instance = req.Url.AbsolutePath
+            };
+
+            _logger.LogError($"Exception parsing message: {aisMessage}", ex);
+            var response = req.CreateResponse(HttpStatusCode.UnprocessableEntity);
+            await response.WriteAsJsonAsync(problemDetails);
+            return response;
         }
     }
+}
+
+public record ProblemDetails
+{
+    public object Status { get; set; }
+    public string Type { get; set; }
+    public string Title { get; set; }
+    public string Detail { get; set; }
+    public string Instance { get; set; }
 }
---
+++ api/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace AisParser.net;
+
+public class Program
+{
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .Build();
+
+        host.Run();
+    }
+}
